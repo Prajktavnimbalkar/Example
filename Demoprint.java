287538f0ce008de4c025fc26eb13b160e785d59e
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-02-08 09:06:55 +0530

    Add files via upload

---
+++ Demoprint.java
@@ -0,0 +1,12 @@
+package Examples;
+
+public class Demoprint {
+	public static void main(String[] args) {
+		System.out.println("prajkta");
+		System.out.println(3);
+		System.out.println(3+4);
+		System.out.println(3-4);
+		System.out.println(3*4);
+		System.out.println(3/4);
+	}
+}
