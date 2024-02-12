f2d46d1d156e248a2ff4bf80d5237235b000bf17
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-02-12 09:15:21 +0530

    Add files via upload

---
+++ Ex2.java
@@ -0,0 +1,9 @@
+package Examples;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		System.out.println("sum = " + (12 + 12));
+		System.out.println("second sum = " + (10 + 10));
+		System.out.println("dividation = " + (10 / 1));
+	}
+}
