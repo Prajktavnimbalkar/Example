88299f103a0c515271a98029b09335c3b53661f8
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-02-13 09:22:40 +0530

    Add files via upload

---
+++ Ex3.java
@@ -0,0 +1,17 @@
+package Examples;
+
+public class Ex3 {
+	public static void main(String[] args) {
+		int no;
+		String a;
+		float b;
+		char c;
+		no = 100;
+		a = "prajkta";
+		b = 1.45f;
+		c = 'a';
+		System.out.println("value of no = "+no);
+		System.out.println("value of a = "+ a);
+		System.out.println("value of b ="+b);
+		System.out.println("value of c = "+c);
+}}
