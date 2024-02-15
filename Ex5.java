87254527b2878c95d5d9d1a40489e332559e4934
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-02-15 09:07:35 +0530

    Add files via upload

---
+++ Ex5.java
@@ -0,0 +1,13 @@
+package Examples;
+
+public class Ex5 {
+	public static void main(String[] args) {
+		int a = 3;
+		System.out.println(a);
+		a = a+3;
+		System.out.println(a);
+		a = a*3;
+		System.out.println(a);
+		a= a-10;
+		System.out.println(a);
+}}
