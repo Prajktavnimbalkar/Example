3c70de0cce92b139813e4cdd25b70db61e5aa488
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-02-16 09:07:59 +0530

    Add files via upload

---
+++ Ex6.java
@@ -0,0 +1,11 @@
+package Examples;
+
+public class Ex6 {
+	public static void main(String[] args) {
+	double b = 123.456;
+	char c = 'P';
+	boolean d = true;
+	System.out.println("double = "+b);
+	System.out.println("char = "+c);
+	System.out.println("boolean = "+ d);
+}}
