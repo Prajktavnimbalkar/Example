2a694fd3bbd7077e5e6801e91ef46c8ec511395c
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-02-19 09:08:55 +0530

    Add files via upload

---
+++ Ex7.java
@@ -0,0 +1,14 @@
+package Examples;
+
+public class Ex7 {
+public static void main(String[] args) {
+	int x = 2, y = 4;
+	System.out.println("x");
+	System.out.println("y");
+	System.out.println("x+y");
+	System.out.println(x);
+	System.out.println(y);
+	System.out.println(x+y);
+	System.out.println(x*y);
+	System.out.println(y-x);
+}}
