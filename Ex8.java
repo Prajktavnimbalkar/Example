43b2fc35769e99b9703c7f48682f3d43a9074140
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-02-20 09:10:33 +0530

    Add files via upload

---
+++ Ex8.java
@@ -0,0 +1,11 @@
+package Examples;
+
+public class Ex8 {
+	public static void main(String[] args) {
+	int num1 = 3, num2 = 4;
+	System.out.println(num1 +"+" +num2 + "=" + (num1+num2));
+	System.out.println(num1 +"+" +num2 + "=" + (num1-num2));
+	System.out.println(num1 +"*" +num2 + "=" + (num1*num2));
+	System.out.println(num1 +"/" +num2 + "=" +(num1/num2));
+}
+}
