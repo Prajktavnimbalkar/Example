65110794300018b1c18789258aedcb7e3a26ac40
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-02-21 09:08:39 +0530

    Add files via upload

---
+++ Ex9.java
@@ -0,0 +1,15 @@
+package Examples;
+
+public class Ex9 {
+public static void main(String[] args) {
+	int input = 10;
+	if (input>0) {
+		System.out.println("number is positive");
+	}
+		else if (input <0){
+			System.out.println("number is negative");}
+			else {
+				System.out.println("number is zero");
+				}}
+			}
+
